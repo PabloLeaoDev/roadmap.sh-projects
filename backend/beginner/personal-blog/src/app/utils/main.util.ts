8949e014d10fdd325afa2167d289b742785aad05
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2025-08-10 23:15:58 -0300

    db utils

---
+++ backend/beginner/personal-blog/src/app/utils/main.util.ts
@@ -0,0 +1,5 @@
+import createPath from "./modules/createPath";
+import getCurrentDateFormat from "./modules/getCurrentDateFormat";
+import createDataBase from "./modules/createDataBase";
+
+export { createPath, getCurrentDateFormat, createDataBase };
---
+++ backend/beginner/personal-blog/src/app/utils/modules/createPath.ts
@@ -0,0 +1,18 @@
+import { existsSync, promises as fs } from 'fs';
+
+export default async function createPath(path: string): Promise<void> {
+  try {
+    const isWindows = process.platform === 'win32', 
+          separator = isWindows ? '\\' : '/',
+          cleanPath = path.split(separator);
+
+    if (cleanPath[cleanPath.length - 1].includes('.')) cleanPath.pop(); // remove file
+
+    if (!existsSync(cleanPath.join(separator))) 
+      await fs.mkdir(cleanPath.join(separator), { recursive: true });
+
+    console.log('The path has been created!');
+  } catch (err) {
+    console.error(err);
+  }
+}
