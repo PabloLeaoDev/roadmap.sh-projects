218c5d157d11ed35821b34dfcfe456fdad526d08
PabloLeaoDev <devpablol@gmail.com>
2025-09-11 17:12:45 -0300

    routes upgrade

--- backend/beginner/personal-blog/src/app/routes.ts
+++ backend/beginner/personal-blog/src/app/routes.ts
@@ -13,9 +13,9 @@ router.get('/article/:id', guessController.renderArticle);
 // Adm
 router.get('/login/admin', admController.renderLogin);
 router.get('/admin/edit/:id', authMid, admController.renderEditArticlePainel);
+router.get('/admin', authMid, admController.renderAdmPainel);
 // router.post('/admin/signup', admController.signup);
 router.post('/admin/signin', admController.signin);
-router.post('/admin', authMid, admController.renderAdmPainel);
 router.post('/admin/edit/:id', authMid, admController.editArticle);
 router.post('/admin/new', authMid, admController.createArticle);
 router.post('/admin/delete/:id', authMid, admController.deleteArticle);
