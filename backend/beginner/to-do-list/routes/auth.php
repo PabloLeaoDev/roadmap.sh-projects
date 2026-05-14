9901c8129f525300afff9fdb47b41f8d33ca03ed
PabloLeaoDev <devpablol@gmail.com>
2026-05-14 17:01:03 -0300

    remove `View` app layer

--- backend/beginner/to-do-list/routes/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/auth/login', [UserController::class, 'login']);
-Route::post('/auth/register', [UserController::class, 'register']);
