a3ec74b246b25018d463db3facce90c0b88bb07e
PabloLeaoDev <devpablol@gmail.com>
2026-05-14 17:01:49 -0300

    add `HTTP` tests file and update routes

--- backend/beginner/to-do-list/routes/web.php
+++ backend/beginner/to-do-list/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+Route::resource('/auth', TaskController::class)
+    ->only(['login', 'register']);
+*/
+
+Route::get('/', [TaskController::class, 'index']);
+
+Route::post('/auth/login', [UserController::class, 'login']);
+Route::post('/auth/register', [UserController::class, 'register']);
